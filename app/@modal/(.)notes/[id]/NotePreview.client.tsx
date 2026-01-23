a4ce87838251aff1f4edb1f429e95e9d401ce5c7
smelik81 <artem.smelyanskiy81@gmail.com>
2026-01-24 00:38:52 +0200

    update component Modal

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
 import css from "@/app/@modal/(.)notes/[id]/NotePreview.module.css";
 import { fetchNoteById } from "@/lib/api";
+import Modal from "@/components/Modal/Modal";
 
 function NotePreview() {
   const router = useRouter();
@@ -18,32 +19,35 @@ function NotePreview() {
   } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
+    enabled: !!id,
   });
 
   if (isLoading) return null;
   if (isError || !note) return <p>Error loading note</p>;
 
   return (
-    <div className={css.overlay}>
-      <div className={css.container}>
-        <div className={css.item}>
-          <div className={css.header}>
-            <h2>{note.title}</h2>
-            <span className={css.tag}>{note.tag}</span>
+    <Modal onClose={close}>
+      <div className={css.overlay}>
+        <div className={css.container}>
+          <div className={css.item}>
+            <div className={css.header}>
+              <h2>{note.title}</h2>
+              <span className={css.tag}>{note.tag}</span>
+            </div>
+
+            <p className={css.content}>{note.content}</p>
+
+            <div className={css.date}>
+              {new Date(note.createdAt).toLocaleDateString()}
+            </div>
+
+            <button className={css.backBtn} onClick={close}>
+              ← Back
+            </button>
           </div>
-
-          <p className={css.content}>{note.content}</p>
-
-          <div className={css.date}>
-            {new Date(note.createdAt).toLocaleDateString()}
-          </div>
-
-          <button className={css.backBtn} onClick={close}>
-            ← Back
-          </button>
         </div>
       </div>
-    </div>
+    </Modal>
   );
 }
 
